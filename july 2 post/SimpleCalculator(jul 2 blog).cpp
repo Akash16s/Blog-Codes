803562f8e44d8d7810e2881d3d0d1f12f6e06131
Akash16s <akashs1698@gmail.com>
2018-07-02 20:08:30 +0530

    july 2 post

---
+++ july 2 post/SimpleCalculator(jul 2 blog).cpp
@@ -0,0 +1,13 @@
+//simple calculator
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;     //yes we can also declare variable like this
+    cin>>a;
+    cin>>b;
+    // above two can also be written as
+    //cin>>a>>b;
+    c=a+b;
+    cout<<"The Sum is :"<<c;
+return 0;
+}
